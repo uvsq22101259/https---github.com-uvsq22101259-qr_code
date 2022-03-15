4dd93ee8a9a6fc05b402cbf34888eccfe8513e33
elie kanga <elie.kanga@ens.uvsq.fr>
2022-03-15 21:20:59 +0100

    definition de la fonction rotation

--- qr_code_projet.py
+++ qr_code_projet.py
@@ -1,3 +1,6 @@
+# noir = 1
+# blanc  = 0
+from tracemalloc import stop
 import PIL as pil
 from PIL import Image
 from PIL import ImageTk 
@@ -5,6 +8,7 @@ from PIL import ImageTk
 
 def nbrCol(matrice):
     return(len(matrice[0]))
+
 
 def nbrLig(matrice):
     return len(matrice)
@@ -18,6 +22,7 @@ def saving(matPix, filename):#sauvegarde l'image contenue dans matpix dans le fi
             toSave.putpixel((j,i),matPix[i][j])
     toSave.save(filename)
 
+
 def loading(filename):#charge le fichier image filename et renvoie une matrice de 0 et de 1 qui représente 
 					  #l'image en noir et blanc
     toLoad=pil.Image.open(filename)
@@ -27,11 +32,35 @@ def loading(filename):#charge le fichier image filename et renvoie une matrice d
             mat[i][j]= 0 if toLoad.getpixel((j,i)) == 0 else 1
     return mat
 
-# noir = 1
-# blanc  = 0
-h = loading("coin.png")
-for i in h:
-    print(i)
+
+def squellette():
+    matrice =loading("coin.png")
+    return matrice
+
+
+def chek_coin(matrice):
+    matrice = loading(matrice)
+    test = squellette()
+    chek = 0
+    # print(matrice[0][17:])
+    # print(test[0][: :-1])
+    for i in range (8):
+        if matrice[i][:8] == test[i] and matrice[-i+(-1)][:8] == test[i] and matrice[i][17:] == test[i] [: :-1] :
+            chek += 1
+            pass
+        else: 
+            print("ce n'est pa un bon carré" )
+            break
+    if chek == 8:        
+        print("les qr code est dans la bonne position")
+
+
+def rotation(matrice):
+    matrice = loading(matrice)
+    matrice_retourner = []
+    
 
 
 
+
+rotation("qr_code_ssfiltre_ascii_rotation.png")
