b15f7211955ae6f1ac1fca43b7543489319b6cbe
elie kanga <elie.kanga@ens.uvsq.fr>
2022-04-05 18:27:43 +0200

    decodage( codage de hamming)

--- qr_code_projet.py
+++ qr_code_projet.py
@@ -1,5 +1,5 @@
-# noir = 1
-# blanc  = 0
+# noir = 0
+# blanc  = 1
 from tracemalloc import stop
 import PIL as pil
 from PIL import Image
@@ -38,7 +38,7 @@ def squellette():
     return matrice
 
 
-def chek_coin(matrice):
+def check_coin(matrice):
     matrice = loading(matrice)
     test = squellette()
     chek = 0
@@ -54,31 +54,79 @@ def chek_coin(matrice):
         print("les qr code est dans la bonne position")
 
 
-def rotation_v(matrice):
+def check_alternance(matrice):
+    alternance = [1,0,1,0,1,0,1,0,1,0,1]
     matrice = loading(matrice)
-    matrice.reverse()
-    saving(matrice,"essai_2.png")
-    
+    verif = True
+    for i in range (11):
+        if matrice[6][7+i] == alternance[i] and matrice[7+i][6] == alternance[i]  :
+            verif = True
+            print("ca marche")
+        else : 
+            print("nope")
+            matrice = rotate(matrice)
+
+ 
+        
+
+
+def rotate(matrice):
+    mat=[]
+    for i in range(nbrCol(matrice)):
+        ligne = []
+        for j in range(nbrLig(matrice)):
+            ligne.append(matrice[j][i])
+        ligne.reverse()
+        mat.append(ligne)
+    matrice = list(mat)
     return matrice
 
 
-def rotation_h(matrice):
-    for i in range (len(matrice)):
-        matrice[i].reverse()
-    return matrice
+def decode(l):
+    d1,d2,d3,d4,p1,p2,p3 = l
+    c = [True,True,True]
+    correction = ""
+    if p1 != ((d1 + d2 + d4) % 2):
+        c[0] = False
+    if p2 != ((d1 + d3 + d4) % 2):
+        c[1] = False
+    if p3 != ((d2 + d3 + d4) % 2):
+        c[2] = False
     
-    
-def rotate(mat):
-    matrotate=[]
-    for i in range(nbrLig(mat)):
-        a = []
-        for j in range(nbrCol(mat)):
-            a.append(mat[nbrLig(mat)-j-1][i])
-        matrotate.append(a)
-    mat = list(matrotate)
-    return mat
+    if False in c:
+        if c.count(False) == 3:
+            d4 = 1 - d4
+            correction = "d4"
+        elif c.count(False) == 2:
+            if c[0] == False and c[1] == False:
+                d1 = 1 - d1
+                correction = "d1"
+            elif c[0] == False and c[2] == False:
+                d2 = 1 - d2
+                correction = "d2"
+            elif c[1] == False and c[2] == False:
+                d3 = 1 - d3
+                correction = "d3"
+        elif c.count(False) == 1 :
+            if c[0] == False:
+                p1 = 1 - p1
+                correction = "p1"
+            if c[1] == False:
+                p2 = 1 - p2
+                correction = "p2"
+            if c[2] == False:
+                p3 = 1 - p3
+                correction = "p3"
+    else : 
+        correction = "aucune correction"
+    l = d1,d2,d3,d4,p1,p2,p3
+    d = d1,d2,d3,d4
+    print (correction)
+    return(d)
+a = decode([0, 1, 1, 0, 1, 1, 0])
+print(a)
 
 
 
 
-chek_coin("qr_code_ssfiltre_ascii_rotation.png")
+# check_alternance("qr_code_ssfiltre_ascii_rotation.png")
